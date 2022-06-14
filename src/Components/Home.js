be91498de29165d997b72f31e5eb0129ea046159
Bram-4197 <94552705+Bram-4197@users.noreply.github.com>
2022-06-14 10:19:08 +0200

    update info

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,7 @@ function Home() {
   return (
     <div className="p-7 text-1xl font-semibold flex-1 h-screen bg-center text-white flex flex-col">
         <h1 className="text-2xl">Onze Game:</h1><br />
+        <div>is een runner game</div>
     </div>
   )
 }
